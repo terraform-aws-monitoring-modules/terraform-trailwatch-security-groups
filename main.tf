8256f1c4826ae38163a731fcb11699b06a2f5701
Wes Murray <114347469+wescmx@users.noreply.github.com>
2024-10-29 14:38:46 +0200

    [1.x] Updated resource names and `alarm_description`.

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_metric_filter" "sg_metric_filter" {
+resource "aws_cloudwatch_log_metric_filter" "metric_filter" {
   count          = length(var.sg_ids)
   log_group_name = var.cw_log_group_name
   name           = "${var.sg_ids[count.index]}-metric-filter"
@@ -11,7 +11,7 @@ resource "aws_cloudwatch_log_metric_filter" "sg_metric_filter" {
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "elb_metric_filter_alarm" {
+resource "aws_cloudwatch_metric_alarm" "metric_filter_alarm" {
   count               = length(var.sg_ids)
   alarm_name          = "${var.sg_ids[count.index]}-metric-filter-alarm"
   comparison_operator = var.cw_metric_filter_alarm_comparison_operator
@@ -21,6 +21,6 @@ resource "aws_cloudwatch_metric_alarm" "elb_metric_filter_alarm" {
   period              = var.cw_metric_filter_alarm_period
   statistic           = var.cw_metric_filter_alarm_statistic
   threshold           = var.cw_metric_filter_alarm_threshold
-  alarm_description   = "Alarm when Security Group ${var.sg_ids[count.index]} exceeds the specified threshold."
+  alarm_description   = "Alarm when the resource exceeds the specified threshold."
   alarm_actions       = var.cw_metric_filter_alarm_actions
 }
